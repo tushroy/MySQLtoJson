21c7fb110f4fede7eabaac6e533f9506131b43ce
Tushar Roy <xxcrack@gmail.com>
2013-04-30 13:14:53 +0600

    script improvements
    improved the script to work with CRUD operations along with select
    operation that returns a json object

--- index.php
+++ index.php
@@ -1,32 +1,36 @@
 <?php
 include_once("jsondbcfg.php");
 header('Content-Type: application/json');
-//$typ=$_GET['t']; //select
-$typ='select';
+$typ=$_GET['t']; //select
+//$typ='select';
 $qry=$_GET['q']; // * from table1
 $query= stripslashes($typ." ".$qry); //"SELECT * from table1";
-//Establish connection
-$con = mysql_connect($mySQLserver,$mySQLuser,$mySQLpass);
+//Establish connection to DB
+$con = mysqli_connect($mySQLserver,$mySQLuser,$mySQLpass,$mySQLdb);
 // Check connection
 if (mysqli_connect_errno())
 {
-	$errorR['name']="Failed to connect to mysql ".mysqli_connect_error();
-	//echo json_encode(errorR);
-	die(json_encode(errorR));
+	$msg['error']="Failed to connect to mysql ".mysqli_connect_error();
+	die(json_encode($msg));
 }
-//Select Database
-mysql_select_db($mySQLdb, $con);
 //Perform query
-$rs= mysql_query($query);
-//Fetching and Iterating Datas
-$arr[]= array();
-while($obj = mysql_fetch_assoc($rs)) // or mysql_fetch_object();
+if(!$rs= mysqli_query($con,$query))
 {
+	mysqli_close($con);
+	$msg['error']="Error in query. ".$query;
+	die(json_encode($msg));
+}
+//Fetching and Iterating Datas on select statement
+if($typ=="select")
+{
+	$arr[]= array();
+	while($obj = mysqli_fetch_assoc($rs))
+	{
 	$arr[] = $obj;
+	}
+//JSON Encode
+	echo '{"data":'.json_encode($arr).'}';
 }
 //Close connection
-mysql_close($con);
-//JSON Encode
-echo '{"data": '.json_encode($arr).'}'; //for returning as an json object of json array. array name "data"
-//echo json_encode($arr); //for returning as an json array of json objects
+mysqli_close($con);
 ?>
--- jsondbcfg.php
+++ jsondbcfg.php
@@ -1,6 +1,6 @@
 <?php
 $mySQLserver="localhost"; //database host
+$mySQLdb="database-name"; //database name
 $mySQLuser="username"; //database user name
 $mySQLpass="password"; //database password
-$mySQLdb="database-name"; //database name
 ?>
